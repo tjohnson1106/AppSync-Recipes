a8eec7b3bdae3a783b4094ea4c9a24728a5a6d94
Thomas Johnson <tjohnson1106@gmail.com>
2018-09-04 13:12:33 -0400

    created initial apollo provider config

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://tqlratl24bdfzanrz3q7tky7bm.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-mqnpejcxzjb7vagxezspazmndm"
+}
