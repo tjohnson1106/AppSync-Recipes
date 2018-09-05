53e26d6d98af778942b8e8df4ff59b338c8afe83
Thomas Johnson <tjohnson1106@gmail.com>
2018-09-05 14:48:17 -0400

    created nav component

---
+++ src/Nav.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { css } from "glamor";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div {...css(styles.container)}>
+        <h1 {...css(styles.heading)} />
+        <Link to="/" {...css(styles.link)}>
+          Recipes
+        </Link>
+        <Link to="/addrecipe" {...css(styles.link)}>
+          Add Recipe
+        </Link>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  link: {
+    textDecoration: "none",
+    marginLeft: 15,
+    color: "white",
+    hover: {
+      textDecoration: "underline"
+    }
+  },
+
+  container: {
+    display: "flex",
+    backgroundColor: "#00c334",
+    padding: "0px 30px",
+    alignItems: "center"
+  },
+  heading: {
+    color: "white",
+    paddingRight: 20
+  }
+};
+
+export default Nav;
